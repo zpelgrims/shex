206d97480df600796592d61e904c8270df0acd74
Zeno Pelgrims <zenopelgrims@gmail.com>
2018-04-02 22:15:48 +0200

    cleaner layout

--- shading_reference_versions.py
+++ shading_reference_versions.py
@@ -17,18 +17,15 @@ def get_reference_version(reference_name):
     
     # strip all non-digits
     reference_stripped_version_number = int(re.sub("\D", "", reference_version_number))
-    
-    
-    
-    return [reference_stripped_version_number, full_folder_path]
 
+    return [reference_stripped_version_number, full_folder_path]
 
 
 def get_folder_latest_version(full_folder_path):
     
     # get all .ma files in folder
     folder_maya_files_list = [f for f in listdir(full_folder_path) if isfile(join(full_folder_path, f)) and f.endswith(".ma")]
-    
+
     folder_latest_file_version = 0
     
     for j in folder_maya_files_list:
@@ -47,27 +44,23 @@ def get_folder_latest_version(full_folder_path):
     return folder_latest_file_version
 
 
-
-
-
 def window():
     
     windowName = "shading_reference_versions"
     windowSize = (600, 300)
     buttonSize = (100, 20)
-    spacing = [(2,20), (3,20), (4, 20)]
+    spacing = [(1,20), (2,20), (3,20)]
 
 
     if (cmds.window(windowName , exists=True)):
         cmds.deleteUI(windowName)
     window = cmds.window( windowName, title= windowName, sizeable=True)
-    cmds.rowColumnLayout( numberOfColumns=4, columnWidth=[(1, 300), (2, 50), (3, 50), (4, 100)], rowSpacing=spacing, columnSpacing=spacing)
+    cmds.rowColumnLayout( numberOfColumns=3, columnWidth=[(1, 300), (2, 50), (3, 50)], rowSpacing=spacing, columnSpacing=spacing)
     
     
     cmds.text( label="Reference name", align='center' )
     cmds.text( label="Current", align='right' )
     cmds.text( label="Latest", align='right' )
-    cmds.text(label="")
     
     reference_list = cmds.ls(references=True)
     
@@ -75,17 +68,16 @@ def window():
         reference_version = get_reference_version(i)
         folder_latest_version = get_folder_latest_version(reference_version[1])      
 
-    
-        cmds.text( label=i, align='left' )
-        cmds.text( label=reference_version[0], align='right' )
-        cmds.text( label=folder_latest_version, align='right' )
-        
+            
         if reference_version[0] < folder_latest_version:
             print str(i) + ": Outdated" + " ----> " + "version " + str(reference_stripped_version_number) + " < " + str(folder_latest_version) 
-            cmds.button(label="Update", bgc=[1, .2, .2])
+            cmds.button( label=i, enable=False, bgc=[1, .2, .2] )
         else:
-            cmds.button(label="Up to date", enable=False, bgc=[0, 1, .5])
+            cmds.button( label=i, enable=False, bgc=[0, 1, .5])
+            
         
+        cmds.text( label=reference_version[0], align='right' )
+        cmds.text( label=folder_latest_version, align='right' )
 
 
     cmds.showWindow( windowName )
